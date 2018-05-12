96e61180a10dd66844732ee2108ddd14280c9691
nhuuhuy <nguyenhuuhuy02@gmail.com>
2018-05-12 13:33:07 +0700

    fix url image font

--- webpack.config.js
+++ webpack.config.js
@@ -69,15 +69,23 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif|mp4)$/,
-                use: [
-                    "file-loader?name=[hash:6].[ext]&outputPath=../images/",
-                ]
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        name: "images/[name]-[hash:8].[ext]",
+                        outputPath: "./images/"
+                    }
+                }]
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    "file-loader?name=[hash:6].[ext]&outputPath=./fonts/",
-                ]
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        name: "fonts/[name]-[hash:8].[ext]",
+                        outputPath: "./fonts/"
+                    }
+                }]
             }
         ]
     },
